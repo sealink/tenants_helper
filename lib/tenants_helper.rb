b49f52e557a6d1844e2a9312e139c2c52990e6f5
Akil Madan <akil.madan@outlook.com>
2016-02-09 09:00:20 +1030

    Allow the config_path to be set up in advance by the calling app. Default to using that value if no value is passed in.

--- lib/tenants_helper.rb
+++ lib/tenants_helper.rb
@@ -1,9 +1,13 @@
 module TenantsHelper
   class << self
-    def tenants(config_path:)
+    def tenants(config_path: @config_path_string)
       config_loader = TenantsHelper::ConfigLoader.new(config_path: config_path)
       config_content = config_loader.load_content
       TenantsHelper::Tenants.new(tenants_config_hash: config_content)
+    end
+
+    def set_path(config_dir:, config_filename: 'tenants.yml')
+      @config_path_string = config_dir + '/' + config_filename
     end
   end
 
--- spec/tenants_helper_spec.rb
+++ spec/tenants_helper_spec.rb
@@ -16,15 +16,27 @@ describe TenantsHelper do
     let(:config_dir) { '/workspace/test_project' }
     let(:config_file) { 'test_config.yml' }
     let(:config_path) { config_dir + '/' + config_file }
-    let(:tenants) { TenantsHelper.tenants config_path: config_path }
     before do
       yaml_loader_double = double
       allow(Yamload::Loader).to receive(:new).and_return(yaml_loader_double)
       allow(yaml_loader_double).to receive(:content).and_return(tenant_config_hash)
       allow_any_instance_of(Pathname).to receive(:exist?).and_return(true)
     end
-    it 'should load tenants successfully and return Tenants object' do
-      expect(tenants).to be_a TenantsHelper::Tenants
+
+    context 'setting the config_path in advance' do
+      let(:default_filename) { 'tenants.yml' }
+      let(:set_path) { TenantsHelper.set_path(config_dir: config_dir) }
+      let(:tenants) { TenantsHelper.tenants }
+      it 'should load tenants successfully and return Tenants object' do
+        expect(set_path).to eq "#{config_dir}/#{default_filename}"
+        expect(tenants).to be_a TenantsHelper::Tenants
+      end
+    end
+    context 'passing the config_path explicitely to tenants method' do
+      let(:tenants) { TenantsHelper.tenants config_path: config_path }
+      it 'should load tenants successfully and return Tenants object' do
+        expect(tenants).to be_a TenantsHelper::Tenants
+      end
     end
   end
 end
